f887bce7413b90c6814a286a9354f0da67306774
Josh Long <joshua.long@broadcom.com>
2026-08-06 00:37:23 -0700

    automatic save @ 2026-08-06T07:37:23.202150Z

---
+++ gateway/src/main/java/com/example/gateway/RouteConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.gateway;
+
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.filter.TokenRelayFilterFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+@Configuration
+class RouteConfiguration {
+
+    // <.>
+    @Bean
+    @Order(Ordered.LOWEST_PRECEDENCE)
+    RouterFunction<ServerResponse> ui() {
+        return route()
+                .GET("/**", http())
+                .before(BeforeFilterFunctions.uri("http://localhost:8020"))
+                .build();
+    }
+
+    // <.>
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    RouterFunction<ServerResponse> api() {
+        return route()
+                .GET("/api/**", http())
+                .before(BeforeFilterFunctions.uri("http://localhost:8081"))
+                .before(BeforeFilterFunctions.rewritePath("/api", "/")) // <.>
+                .filter(TokenRelayFilterFunctions.tokenRelay())  // <.>
+                .build();
+    }
+
+
+}
